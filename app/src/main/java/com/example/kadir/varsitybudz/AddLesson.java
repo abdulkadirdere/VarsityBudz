22cb6f03aa6af458da1c64013283a3f04fb2537c
abdulkadirdere <752817@students.wits.ac.za>
2017-08-28 23:45:41 +0200

    create lesson in complete with dialog box

--- app/src/main/java/com/example/kadir/varsitybudz/AddLesson.java
+++ app/src/main/java/com/example/kadir/varsitybudz/AddLesson.java
@@ -1,9 +1,14 @@
 package com.example.kadir.varsitybudz;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -15,7 +20,35 @@ public class AddLesson extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_lesson);
+        createLesson();
     }
+
+    public Button btnCreateLesson;
+
+    public void createLesson(){
+        btnCreateLesson = (Button) findViewById(R.id.createLesson);
+        btnCreateLesson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createDialog();
+            }
+        });
+
+    }
+
+    private void createDialog(){
+        AlertDialog.Builder alertDiag = new AlertDialog.Builder(this);
+        alertDiag.setMessage("Lesson has been created");
+        alertDiag.setPositiveButton("Confirm",new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                Intent ceateLesson = new Intent(AddLesson.this,MainActivity.class);
+                startActivity(ceateLesson);            }
+
+        });
+        alertDiag.create().show();
+    }
+
 
 
 }
