96ea2c9f9653f0cc94684a0ff641d7d2dd7796a9
abdulkadirdere <752817@students.wits.ac.za>
2017-08-28 13:58:16 +0200

    teacher and student activities created with access

--- app/src/main/java/com/example/kadir/varsitybudz/MainActivity.java
+++ app/src/main/java/com/example/kadir/varsitybudz/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.kadir.varsitybudz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        profBtnInit();
+        studentBtnInit();
+    }
+
+    //prof button
+    public Button btnStudent;
+    public Button btnProf;
+
+    public void profBtnInit() {
+        btnProf = (Button) findViewById(R.id.prof);
+        btnProf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addLesson = new Intent(MainActivity.this,AddLesson.class);
+                startActivity(addLesson);
+            }
+        });
+    }
+    public void studentBtnInit(){
+        btnStudent = (Button) findViewById(R.id.student);
+        btnStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent selectLesson = new Intent(MainActivity.this, SelectLesson.class);
+                startActivity(selectLesson);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/kadir/varsitybudz/SelectLesson.java
@@ -0,0 +1,18 @@
+package com.example.kadir.varsitybudz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SelectLesson extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_lesson);
+    }
+
+    public void btnSelectMath(){
+        //btnSelectMath = (Button) find
+    }
+}
